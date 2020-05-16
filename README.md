05a7a3e684cfc6a7edf96e7e18b77a8308bcf95d
Kyuho Lee <modulo641@gmail.com>
2020-05-16 18:59:39 +0900

    Version 0.3.0
    Configurable settings

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ A Foundry Virtual Tabletop Module that makes /stream view suitable for web brows
 ## How to use
 
 1. Install and activate this module.
-2. Go to <your-hostname>/stream view page or just click the **Print** button, which is next to the **Export Chat Log** button.
-3. Print the /stream view page with browser's print function.
+2. Go to the \<your-hostname>/stream page or just click the **Print** button, which is next to the **Export Chat Log** button.
+3. Print the /stream view page with your web browser's print function.
--- printable-stream.js
+++ printable-stream.js
@@ -15,12 +15,51 @@ class PrintableStreamModule {
 
     window.location.href = '/stream';
   }
+
+  static addStyle(styleString) {
+    const style = document.createElement('style');
+    style.textContent = styleString;
+    document.head.append(style);
+  }
 }
+
+// Settings
+Hooks.once('init', () => {
+  game.settings.register('PrintableStream', 'hideButtons', {
+    name: game.i18n.localize("printable-stream.hide-buttons-s"),
+    hint: game.i18n.localize("printable-stream.hide-buttons-l"),
+    scope: "client",
+    config: true,
+    default: false,
+    type: Boolean
+  });
+
+  game.settings.register('PrintableStream', 'fitwidth', {
+    name: game.i18n.localize("printable-stream.fitwidth-s"),
+    hint: game.i18n.localize("printable-stream.fitwidth-l"),
+    scope: "client",
+    config: true,
+    default: true,
+    type: Boolean
+  });
+
+  game.settings.register('PrintableStream', 'bgColor', {
+    name: game.i18n.localize("printable-stream.bgcolor-s"),
+    hint: game.i18n.localize("printable-stream.bgcolor-l"),
+    scope: "client",
+    config: true,
+    default: "transparent",
+    type: String,
+  });
+});
 
 Hooks.on('init', () => {
   const url = window.location.pathname;
   if (/\/stream/.test(url)) {
+    // Increase ChatMessage batch size to render all messages
     CONFIG.ChatMessage.batchSize = Number.MAX_SAFE_INTEGER;
+
+    // Inject css
     let cssId = 'modules/PrintableStream/printable-stream.css';
     if (!document.getElementById(cssId)) {
         let head  = document.getElementsByTagName('head')[0];
@@ -32,6 +71,34 @@ Hooks.on('init', () => {
         link.media = 'all';
         head.appendChild(link);
     }
+
+    // Apply settings
+    const hideButtons = game.settings.get('PrintableStream', 'hideButtons');
+    const fitwidth = game.settings.get('PrintableStream', 'fitwidth');
+    const bgColor = game.settings.get('PrintableStream', 'bgColor');
+
+    if (hideButtons) {
+      PrintableStreamModule.addStyle(`
+        button {
+          display: none;
+        }
+      `);
+    }
+
+    if (fitwidth) {
+      PrintableStreamModule.addStyle(`
+        body.stream .tab[data-tab] {
+          width: 100%;
+        }
+      `);
+    }
+
+    PrintableStreamModule.addStyle(`
+      body.stream {
+        background: ${bgColor};
+      }
+    `);
   }
 });
+
 Hooks.on('renderChatLog', PrintableStreamModule.onRenderChatLog);
